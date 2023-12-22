b163251f41f50e39a27f8a227245c582bcb0c517
max <max.iw15@yandex.ru>
2023-12-22 10:52:00 +0300

    initial commit and finish

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Stars from "./components/Stars.tsx";
+
+function App() {
+    return (
+        <Stars count={1}></Stars>
+    );
+}
+export default App
---
+++ src/components/Stars.tsx
@@ -0,0 +1,26 @@
+import Star from "./Star.tsx";
+
+import { nanoid } from 'nanoid'
+
+type starCount = {
+    count: number
+}
+export const Stars = ({ count = 0 }: starCount) => {
+    if (Number.isNaN(count)) return null;
+    if (count < 1 || count > 5) return null;
+
+    const starsListItems = [];
+
+    for (let i = 1; i <= count; i += 1) {
+        starsListItems.push(<li key={nanoid()}><Star /></li>);
+    }
+
+    return (
+        <ul className="card-body-stars">
+            {[starsListItems]}
+        </ul>
+    )
+}
+
+
+export default Stars;
